1462bdaa53b7d0b75b92d1c3193fa78511859c98
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-02-10 14:31:31 +0100

    add card and change favicon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import AppHeader from './components/AppHeader'
 
 import getCharacters from './services/getCharacters'
 
+import Card from './components/Card'
+import createElement from './lib/createElement'
+
 export default function App() {
   const header = AppHeader('Harry Potter App')
   document.body.append(header)
@@ -9,6 +12,20 @@ export default function App() {
   // fetch API
 
   getCharacters()
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+    .then(characters => createCards(characters))
+    .catch(error => handleGetCharacterError(error))
+
+  function createCards(characters) {
+    const cards = characters.map(character => Card(character.name))
+    document.body.append(...cards)
+  }
+
+  function handleGetCharacterError(error) {
+    const errorMessage = createElement(
+      'strong',
+      { style: 'color: crimson;' },
+      error.message
+    )
+    document.body.append(errorMessage)
+  }
 }
